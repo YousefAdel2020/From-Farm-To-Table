e662521399bd0abf0f216dd818bb571ca76ea858
Yousef Adel <yousefjooo2018@gmail.com>
2023-05-11 12:18:57 +0300

    add the login and register functionality

--- backend/middleware/errorHandlerMiddleware.js
+++ backend/middleware/errorHandlerMiddleware.js
@@ -3,12 +3,12 @@ const {CustomAPIError}=require('../errors');
 const errorHandler=(err,req,res,next)=>{
     if(err instanceof CustomAPIError)
     {
-        console.log('in error middlware2');
+        console.log('custom error middlware2');
         return res.status(err.statusCode).json({msg:err.message});
        
     }
     console.log('in error middlware3');
-    return res.status(err.status).json({msg:err.message});
+    return res.json({msg:err.message});
 }
 
 module.exports=errorHandler;
