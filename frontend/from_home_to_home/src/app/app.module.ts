bc703275493ae96aed5b0e038e31ba6c4a8c564b
Maysara Safwat <maisarasafwat2@gmail.com>
2023-05-12 22:10:06 +0300

    add home

--- frontend/from_home_to_home/src/app/app.module.ts
+++ frontend/from_home_to_home/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './Components/card/card.component';
 import { DishesComponent } from './Components/dishes/dishes.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    DishesComponent
+    DishesComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
