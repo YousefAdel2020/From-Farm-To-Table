9fce087bc8bf9b258139ebea38a9a85b276c2c9c
weamibrahim <weamibrahim46@gmail.com>
2023-05-15 22:36:46 +0300

    profile chief

--- frontend/from_home_to_home/src/app/app-routing.module.ts
+++ frontend/from_home_to_home/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateChiefComponent } from './Components/update-chief/update-chief.component';
+import { ProfileChiefComponent } from './Components/profile-chief/profile-chief.component';
+import { DishesComponent } from './Components/dishes/dishes.component';
+import { UpdateUserComponent } from './Components/update-user/update-user.component';
+import { ProfileUserComponent } from './Components/profile-user/profile-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'updateChief', component:UpdateChiefComponent},
+  {path:'viewChief', component:ProfileChiefComponent},
+  {path:'dishes', component:DishesComponent},
+  {path:'updateUser', component:UpdateUserComponent},
+  {path:'viewUser', component:ProfileUserComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/from_home_to_home/src/app/app.module.ts
+++ frontend/from_home_to_home/src/app/app.module.ts
@@ -2,15 +2,80 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ProfileUserComponent } from './Components/profile-user/profile-user.component';
+import { UpdateUserComponent } from './Components/update-user/update-user.component';
+
+import { ProfileChiefComponent } from './Components/profile-chief/profile-chief.component';
+
+import { RatingStarComponent } from './Components/rating-star/rating-star.component';
+
+import { UpdateChiefComponent } from './Components/update-chief/update-chief.component';
+import { CardComponent } from './Components/card/card.component';
+import { DishesComponent } from './Components/dishes/dishes.component';
+import { LoginFormComponent } from './Components/login-form/login-form.component';
+import { RegisterPageComponent } from './Components/register-page/register-page.component';
+
+
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+
+
+import { HomeDishesComponent } from './Components/home/home-dishes/home-dishes.component';
+
+
+
+let routes:Routes = [
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path: "register", component:RegisterPageComponent},
+  {path:"login", component:LoginFormComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileUserComponent,
+    UpdateUserComponent,
+    ProfileChiefComponent,
+    RatingStarComponent,
+    CardComponent,
+    DishesComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginFormComponent,
+    RegisterPageComponent,
+    HomeDishesComponent,
+    UpdateChiefComponent
   ],
+  
+
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    RouterModule.forRoot(routes),
+
+    // NgxStarRatingModule,
+    FormsModule,
+
+    BrowserAnimationsModule,
+    MatIconModule,
+  
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
