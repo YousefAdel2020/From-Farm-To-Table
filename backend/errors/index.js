64afe41ce3bcacd56df47a0d4a5d16677b80919a
Yousef Adel <yousefjooo2018@gmail.com>
2023-05-16 16:31:57 +0300

    add login and register functionality

---
+++ backend/errors/index.js
@@ -0,0 +1,12 @@
+//? this file to collect all error files in one file
+const CustomAPIError = require('./custom-api')
+const UnauthenticatedError = require('./unauthorized')
+const NotFoundError = require('./NotFound')
+const BadRequestError = require('./badRequest')
+
+module.exports = {
+  CustomAPIError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+}
