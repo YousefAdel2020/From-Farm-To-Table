1b4462d33e5de9e2a131dc6fe9b775c895e8e6f6
Habiba Alaa <habiba.elganainey@gmail.com>
2023-05-19 09:41:11 +0300

    dishes page

--- frontend/from_home_to_home/src/app/Components/dishes/dishes.component.ts
+++ frontend/from_home_to_home/src/app/Components/dishes/dishes.component.ts
@@ -3,9 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dishes',
   templateUrl: './dishes.component.html',
-  styles: [
-  ]
+  styleUrls: ['./dishes.component.css']
 })
 export class DishesComponent {
+  public nameFilter!: string;
 
+  public products = [
+    {id: 1, name: 'Pasta', content:'love pasta',price: 10},
+    {id: 2, name: 'Pizza', content:'love pizaa', price: 12},
+    {id: 3, name: 'Burger',content:'love pizaa', price: 8},
+    {id: 4, name: 'Salad',content:'love pizaa', price: 6},
+    {id: 5, name: 'Salad',content:'love pizaa', price: 6},
+    {id: 6, name: 'Salad',content:'love pizaa', price: 6},
+    {id: 7, name: 'Salad',content:'love pizaa', price: 6}
+  ];
+ 
 }
