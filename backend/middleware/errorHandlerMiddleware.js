231ba657af2ca737627a0022abdafae8d9854c65
Yousef Adel <yousefjooo2018@gmail.com>
2023-05-22 00:19:27 +0300

    add CastError and Dublicate error handler

--- backend/middleware/errorHandlerMiddleware.js
+++ backend/middleware/errorHandlerMiddleware.js
@@ -1,35 +1,46 @@
-const {CustomAPIError}=require('../errors');
-
-const errorHandler=(err,req,res,next)=>{
-
-
-    let customError = {
-        //set default
-        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: err.message || `something went wrong try again later`,
-      };
-
-    // if(err instanceof CustomAPIError)
-    // {
-    //     console.log('custom error middlware2');
-    //     return res.status(err.statusCode).json({msg:err.message});
-       
-    // }
+const { CustomAPIError } = require("../errors");
+const {StatusCodes} = require('http-status-codes');
 
 
-      //if you register without write email or password
-  if(err.name==='ValidationError')
-  {
-    
-    customError.msg=Object.values(err.errors).map((item)=>item.message).join(', ');
-    
-    customError.statusCode=StatusCodes.BAD_REQUEST;
+
+const errorHandler = (err, req, res, next) => {
+  let customError = {
+    //*set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || `something went wrong try again later`,
+  };
+
+
+
+  //& if you register without write email or password
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+
+    customError.statusCode = StatusCodes.BAD_REQUEST;
     //console.log(customError);
-    
   }
 
-    console.log('in error middlware3');
-    return res.json({msg:err.message});
-}
 
-module.exports=errorHandler;
+    //& handle cast error
+    if(err.name==='CastError')
+    {
+      customError.msg=`No item with Id : ${err.value}`
+      customError.statusCode=StatusCodes.NOT_FOUND
+    }
+    
+    //& if you register with the same email
+    if(err.code&&err.code===11000)
+    {
+      customError.statusCode=StatusCodes.BAD_REQUEST;
+      //object.keys(); return array of object keys 
+      customError.msg=`Dublicate valued entered for ${Object.keys(err.keyValue)} field, please choose another value`
+  
+    }
+
+
+    return res.status(customError.statusCode).json({msg: customError.msg });
+};
+
+module.exports = errorHandler;
