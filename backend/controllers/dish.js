f9d7813cc6ff4c3152cd7d1a8ca99187a488614a
Yousef Adel <yousefjooo2018@gmail.com>
2023-05-24 18:01:40 +0300

    make upload util

--- backend/controllers/dish.js
+++ backend/controllers/dish.js
@@ -1,43 +1,53 @@
 const Dish = require("../models/dishs");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError,NotFoundError } = require("../errors");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
+  ForbidenError,
+} = require("../errors");
 
 const getAllDishes = async (req, res) => {
-  const dishes = await Dish.find({});
+  const dishes = await Dish.find({}).populate(
+    "chef",
+    "firstName lastName email img role gender summary address phoneNumber rating"
+  );
 
   res.status(StatusCodes.OK).json({ dishes });
 };
 
 const createDish = async (req, res) => {
+  if (req.User.role != "chef") {
+    throw new ForbidenError("You can create dish");
+  }
+  if (req.file) {
+    req.body.img = req.file.filename;
+  }
+  req.body.chef = req.user.userId;
   const dish = await Dish.create(req.body);
+
   res.status(StatusCodes.CREATED).json({ dish });
 };
 
-
-const getDish=async (req, res) => {
-  const {dishId}=req.params
+const getDish = async (req, res) => {
+  const { dishId } = req.params;
 
   const dish = await Dish.findOne({
-    _id:dishId
+    _id: dishId,
   });
 
-  if(!dish)
-  {
+  if (!dish) {
     throw new NotFoundError(`there is no Dish with this Id : ${dishId}`);
   }
 
-  res.status(StatusCodes.OK).json({dish});
+  res.status(StatusCodes.OK).json({ dish });
 };
 
-const updateDish=async (req, res) => {
-
-
+const updateDish = async (req, res) => {
   res.status(StatusCodes.OK).json("update dish");
 };
 
-const deleteDish=async (req, res) => {
-
-
+const deleteDish = async (req, res) => {
   res.status(StatusCodes.OK).json("delete dish");
 };
 
@@ -46,5 +56,5 @@ module.exports = {
   createDish,
   getDish,
   updateDish,
-  deleteDish
+  deleteDish,
 };
